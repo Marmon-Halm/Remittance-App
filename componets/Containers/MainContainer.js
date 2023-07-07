615e3cd675c16d04ca494b8454d24a9689f420c1
Marmon-Halm <michaelmarmonhalm30@gmail.com>
2023-07-07 15:54:05 +0000

    topButtonDone

--- componets/Containers/MainContainer.js
+++ componets/Containers/MainContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { styled } from 'styled-components/native';
 import { StatusBarHeight } from '../shared';
 import { color } from '../../screens/color';
@@ -9,7 +10,6 @@ const { white } = color;
 const StyledView = styled.View`
     flex: 1;
     paddingHorizontal: 22px;
-    padding-top: ${StatusBarHeight }px;
     background-color: ${white};
 `;
 
--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Image, Button, ImageBackground, } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image, Button, ImageBackground } from 'react-native';
 import { useFonts, Manrope_400Regular, Manrope_500Medium, Manrope_700Bold, Manrope_600SemiBold, Manrope_800ExtraBold } from '@expo-google-fonts/manrope';
 import { Ionicons } from "@expo/vector-icons";
 import { AntDesign } from '@expo/vector-icons';
 import AppLoading from 'expo-app-loading';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Feather, MaterialIcons } from '@expo/vector-icons';
 import Modal from "react-native-modal";
-import MapView, { PROVIDER_GOOGLE, Marker} from 'react-native-maps'
-import MainContainer from '../componets/Containers/MainContainer';
-import MapContainer from '../componets/Containers/MapContainer';
-
-
+import MapView, { PROVIDER_GOOGLE, Marker, getInitialState } from 'react-native-maps'
+import * as Location from 'expo-location';
 
 // apiKey: AIzaSyA25oUM8BiNy3Iuv4QaLDTU4YzbZxmZUX4
 
@@ -21,10 +20,72 @@ export default function Home(params) {
 
   const [isModalVisible, setIsModalVisible] = React.useState(false);
   const [isModalVisibleGH, setIsModalVisibleGH] = React.useState(false);
-
-
   const handleModal = () => setIsModalVisible(() => !isModalVisible);
   const handleModalGH = () => setIsModalVisibleGH(() => true)
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [pin, setPin] = useState({
+    latitude: 5.614818,
+    longitude: -0.205874
+  });
+
+  // useEffect(() => {
+  //   (async () => {
+
+  //     let { status } = await Location.requestForegroundPermissionsAsync();
+  //     if (status !== 'granted') {
+  //       setErrorMsg('Permission to access location was denied');
+  //       return;
+  //     }
+
+  //     let location = await Location.getCurrentPositionAsync({});
+  //     setLocation(location);
+  //   })();
+  // }, []);
+
+  // let text = 'Waiting..';
+  // if (errorMsg) {
+  //   text = errorMsg;
+  // } else if (location) {
+  //   text = JSON.stringify(location);
+  // }
+
+
+  const [liveLatitude, setLiveLatitude] = useState(null);
+  const [liveLongitude, setLiveLongitude] = useState(null);
+
+  const permissionAlert = () => {
+    Alert.alert(
+      'You did not allow location permissions',
+      'Please go to settings and allow location permissions for full functionality',
+      [
+      ],
+      {
+        cancelable: true
+      }
+    );
+  }
+
+  useEffect(() => {
+    (async () => {
+
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      console.log(location);
+
+      setPin({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      });
+      console.log("Pin: ", pin)
+    })();
+  }, []);
+
 
 
   let [fontsLoaded] = useFonts({
@@ -40,19 +101,42 @@ export default function Home(params) {
     return <AppLoading />;
   }
 
+  // <View style={{height: '100%', position: 'relative'}}>
+
+  return (
+    <SafeAreaView>
+      <View style={{ paddingHorizontal: 0, height: '100%', marginTop: StatusBar.currentHeight }}>
+        <MapView
+          style={{ flex: 1 }}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: 5.710106756927991,
+            longitude: -0.15921827899221827,
+            latitudeDelta: 0.0059,
+            longitudeDelta: 0.0059,
+          }}
+          showsUserLocation={true}>
+
+
+        </MapView>
+
+        <View style={{ width: 40, height: 40, backgroundColor: 'white', position: 'absolute', top: 20, left: 20, borderRadius: 50 / 2, alignItems: "center", justifyContent: "center", borderWidth: 0.5, borderColor: "grey" }}>
+          <Feather name="menu" size={20} color="#000" />
+        </View>
+        <View style={{ width: 40, height: 40, backgroundColor: 'white', position: 'absolute', top: 20, right: 20, borderRadius: 50 / 2, alignItems: "center", justifyContent: "center", borderWidth: 0.5, borderColor: "grey" }}>
+        <MaterialIcons name="my-location" size={24} color="black" />
+        </View>
+        <View style={{ width: "100%", height: "45%", backgroundColor: 'white', position: 'absolute', bottom: 0, borderTopLeftRadius: 20, borderTopRightRadius: 20 }}></View>
+
+        <StatusBar style="dark" />
+      </View>
+    </SafeAreaView>
+
+  )
 
 
 
-  return <MapContainer>
-    <MapView
-      style={{ flex: 1 }}
-      provider={PROVIDER_GOOGLE}
-      region={{ latitude: 42.882004, longitude: 74.582748, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }}
-      showsUserLocation={true} />
 
-    <StatusBar style="dark" />
-
-  </MapContainer>
 }
 
 const styles = StyleSheet.create({
